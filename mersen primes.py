189f03d65b4c095ab54f0133f3d906fabe253fc9
arezaei <83867047+TeraPascal@users.noreply.github.com>
2026-05-13 14:36:14 +0330

    Create mersen primes.py

---
+++ mersen primes.py
@@ -0,0 +1,26 @@
+import datetime
+while True:
+    
+    def prime(l):
+        if len(l) == 2:
+            return(1)
+        else:
+            return(0)
+        
+    def divisors(num):
+        l=[]
+        for i in range(num):
+            i = i+1
+            r = num%i
+            if r == 0 :
+                l.append(i)
+        return(l)
+
+    def mersen(power):
+        num = 2**power - 1
+        l = divisors(num)
+        p = prime(l)
+        if p == 1:
+            print(datetime.datetime.now(),f"\n {num} is mersen prime")
+    
+    mersen(int(input('power :\n  >  ')))
